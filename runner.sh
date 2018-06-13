7df2f19853474a3735ce5d00555e3d19abcef336
deepakd6 <38915794+deepakd6@users.noreply.github.com>
2018-06-13 15:29:57 +0530

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-sleep 40s &&
+sleep 40s &&  # Making the script sleep for 40sec and Run the following commands after sleep
 
-DB_USER='root';
-DB_PASSWD='root123';
-DB_HOST='172.17.0.2'
-DB_NAME='lrt_mfstats';
-TABLE='20180507_tfp_gaps_index';
+DB_USER='root';         # Variable holding the username of  mysql
+DB_PASSWD='root123';    # Variable holding the Password of  mysql 
+DB_HOST='172.17.0.2'    # Mysql container IP as Host to connect.
+DB_NAME='lrt_mfstats';  # Database Name in the mysql
+TABLE='20180507_tfp_gaps_index';    # Table in the database.
 
 
 #mysql commands
 
 
-
+# Running a while loop infintely to insert the data 
 while true
 do
-
+#Inserting data into table
 mysql --user=$DB_USER --password=$DB_PASSWD $DB_NAME -h $DB_HOST -e "INSERT INTO $TABLE VALUES (UNIX_TIMESTAMP(now()) ,'cert_pa','cert_pa',FLOOR(1 + (RAND() * 9999)),FLOOR(1 + (RAND() * 9999)),FLOOR(1 + (RAND() * 9999)))"
-mysql --user=$DB_USER --password=$DB_PASSWD $DB_NAME -e "DO SLEEP(10)"
+
+# Pause the insertion of data into the table for 10 sec
+mysql --user=$DB_USER --password=$DB_PASSWD $DB_NAME -e "DO SLEEP(10)"  
 
 done
