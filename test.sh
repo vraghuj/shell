7bfcfcfabfdab5c5c958735b88316a65a75d8b2a
deepakd6 <38915794+deepakd6@users.noreply.github.com>
2018-06-13 15:22:51 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-var_sql=sql-441
-var_spr=spr-441
-sql_name=sqldb-441
-container_name=madamdb-441
+var_sql=sql-441                # Variable holding sql dockerfile name.
+var_spr=spr-441                # Variable holding spring-boot dockerfile name
+sql_name=sqldb-441             # Variable holding the sql container name.
+container_name=madamdb-441     # variable holding spring-boot container name
 
 
 
-docker build -f Sqldockerfile -t $var_sql .
-docker build -f dockerfile -t $var_spr .
-docker run -d --name=$sql_name $var_sql
-docker ps
-docker run -p 8080:8080 --name $container_name --link $sql_name:mysql -d $var_spr
+docker build -f Sqldockerfile -t $var_sql .    # Building the docker image for the sql dockerfile
+docker build -f dockerfile -t $var_spr .       # Building the docker image for the spring-boot dockerfile
+docker run -d --name=$sql_name $var_sql        # Running the mysql container in background.
+docker ps                                      # To display the active running containers
 
 
+
+#Mapping the container port 8080 to host port 8080 and linking the sql and spring-boot containers and running them.
+docker run -p 8080:8080 --name $container_name --link $sql_name:mysql -d $var_spr 
+
+
+#To Display the active running containers
 docker ps
